4cd160fa66bfaa63d3be5c24a0928eb418d146a6
JRSumner <joeraysumner@gmail.com>
2022-02-14 19:46:32 +0000

    refactors app.js

--- db/app.js
+++ db/app.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const { getArticles } = require("./controllers/articles.controller");
 const { getTopics } = require("./controllers/topics.controller");
 const app = express();
 
 app.get("/api/topics", getTopics);
+
+app.get("/api/articles/:article_id", getArticles);
 
 app.all("/*", (req, res) => {
   res.status(404).send({ message: "path not found" });
