61a6896e54780cdca728640dd233ab90612cf749
JRSumner <joeraysumner@gmail.com>
2022-02-14 15:05:47 +0000

    returns all topics from the database to the controller

--- db/models/topics.model.js
+++ db/models/topics.model.js
@@ -1,3 +1,13 @@
+const { response } = require("../app.js");
 const db = require("../connection.js");
 
-exports.fetchTopics = () => {};
+exports.fetchTopics = () => {
+  return db
+    .query("SELECT * FROM topics;")
+    .then((response) => {
+      return response.rows;
+    })
+    .catch((err) => {
+      console.log(err, "in model");
+    });
+};
