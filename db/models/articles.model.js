1e8ecdd1ce2dc1def15955d260ac45b47f102f83
JRSumner <joeraysumner@gmail.com>
2022-02-15 10:51:41 +0000

    destructors fetchArticles and refactors param names

--- db/models/articles.model.js
+++ db/models/articles.model.js
@@ -4,9 +4,8 @@ exports.fetchArticles = (id) => {
   return db
     .query(`SELECT * FROM articles WHERE article_id = $1`, [id])
     .then((response) => {
-      console.log(response.rows[0]);
-      const articles = { articles: response.rows[0] };
-      return articles;
+      const article = { articles: response.rows[0] };
+      return article;
     })
     .catch((err) => {
       console.log(err);
