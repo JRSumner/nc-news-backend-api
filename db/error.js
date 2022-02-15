7e7a6222cb29e4ccc0501bca899c7730baa36431
JRSumner <joeraysumner@gmail.com>
2022-02-15 12:00:02 +0000

    renames handlesArticlesNotFound to handlesCustomErrors

--- db/error.js
+++ db/error.js
@@ -6,7 +6,7 @@ exports.handlesBadRequest = (err, req, res, next) => {
   res.status(404).send({ message: "no article matching that id" });
 };
 
-exports.handlesArticleNotFound = (err, req, res, next) => {
+exports.handlesCustomErrors = (err, req, res, next) => {
   if (err.status && err.msg) {
     res.status(err.status).send({ msg: err.msg });
   }
