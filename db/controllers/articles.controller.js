82d92d911049221dba5b34b8a401f829be4e7ef4
JRSumner <joeraysumner@gmail.com>
2022-02-15 16:18:43 +0000

    adds patchVotes function

--- db/controllers/articles.controller.js
+++ db/controllers/articles.controller.js
@@ -1,9 +1,24 @@
+const { response } = require("../app.js");
+const { updateVotes } = require("../models/articles.model.js");
 const { fetchArticles } = require("../models/articles.model.js");
+
 exports.getArticles = (req, res, next) => {
   const id = req.params.article_id;
   fetchArticles(id)
     .then((article) => {
       res.status(200).send(article);
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+
+exports.patchVotes = (req, res, next) => {
+  const votes = req.body.votes;
+  const id = req.params.article_id;
+  updateVotes(votes, id)
+    .then((response) => {
+      res.status(200).send(response);
     })
     .catch((err) => {
       next(err);
