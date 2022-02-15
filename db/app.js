405b81a4162b2e2fd916cd923b2316d5cbc38b61
JRSumner <joeraysumner@gmail.com>
2022-02-15 16:17:46 +0000

    adds patch path for /api/articles/articles_id

--- db/app.js
+++ db/app.js
@@ -1,12 +1,19 @@
 const express = require("express");
-const { getArticles } = require("./controllers/articles.controller");
+const {
+  getArticles,
+  patchVotes,
+} = require("./controllers/articles.controller");
 const { getTopics } = require("./controllers/topics.controller");
 const { handlesBadPath, handlesCustomErrors } = require("./error");
 const app = express();
 
+app.use(express.json());
+
 app.get("/api/topics", getTopics);
 
 app.get("/api/articles/:article_id", getArticles);
+
+app.patch("/api/articles/:article_id", patchVotes);
 
 app.all("/*", handlesBadPath);
 
