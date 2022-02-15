c8f2ab4082fc1b747b9c442ecbefe7c4be3f2fa3
JRSumner <joeraysumner@gmail.com>
2022-02-15 11:59:42 +0000

    renames handlesArticlesNotFound to handlesCustomErrors

--- db/app.js
+++ db/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { getArticles } = require("./controllers/articles.controller");
 const { getTopics } = require("./controllers/topics.controller");
-const { handlesBadPath, handlesArticleNotFound } = require("./error");
+const { handlesBadPath, handlesCustomErrors } = require("./error");
 const app = express();
 
 app.get("/api/topics", getTopics);
@@ -10,6 +10,6 @@ app.get("/api/articles/:article_id", getArticles);
 
 app.all("/*", handlesBadPath);
 
-app.use(handlesArticleNotFound);
+app.use(handlesCustomErrors);
 
 module.exports = app;
