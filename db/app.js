de3a5fecdc9eb1baa01f06ababc0b85c72e6c468
JRSumner <joeraysumner@gmail.com>
2022-02-15 11:45:31 +0000

    adds handlesArticleNotFound function from errors.js

--- db/app.js
+++ db/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { getArticles } = require("./controllers/articles.controller");
 const { getTopics } = require("./controllers/topics.controller");
-const { handlesBadPath } = require("./error");
+const { handlesBadPath, handlesArticleNotFound } = require("./error");
 const app = express();
 
 app.get("/api/topics", getTopics);
@@ -9,5 +9,7 @@ app.get("/api/topics", getTopics);
 app.get("/api/articles/:article_id", getArticles);
 
 app.all("/*", handlesBadPath);
+
+app.use(handlesArticleNotFound);
 
 module.exports = app;
