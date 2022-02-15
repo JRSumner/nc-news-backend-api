a50603244b69ff59b19ddc070f86e8b7bfced522
JRSumner <joeraysumner@gmail.com>
2022-02-15 11:46:35 +0000

    creates function to handle when a user has passed a valid article id, however that id is not assosiated with a article

--- db/error.js
+++ db/error.js
@@ -1,3 +1,13 @@
 exports.handlesBadPath = (req, res) => {
   res.status(404).send({ message: "path not found" });
 };
+
+exports.handlesBadRequest = (err, req, res, next) => {
+  res.status(404).send({ message: "no article matching that id" });
+};
+
+exports.handlesArticleNotFound = (err, req, res, next) => {
+  if (err.status && err.msg) {
+    res.status(err.status).send({ msg: err.msg });
+  }
+};
