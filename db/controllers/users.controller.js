eca01049a8bf513a3efa332c2811f38a7419e14f
JRSumner <joeraysumner@gmail.com>
2022-03-19 13:49:50 +0000

    complete ticket-22 add endpoint for get request with username

--- db/controllers/users.controller.js
+++ db/controllers/users.controller.js
@@ -1,9 +1,20 @@
-const { fetchUsers } = require("../models/user.model.js");
+const { fetchUsers, fetchSingleUser } = require("../models/user.model.js");
 
 exports.getUsers = (req, res, next) => {
   fetchUsers()
     .then((users) => {
       res.status(200).send(users);
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+
+exports.getSingleUser = (req, res, next) => {
+  const username = req.params.username;
+  fetchSingleUser(username)
+    .then((user) => {
+      res.status(200).send(user);
     })
     .catch((err) => {
       next(err);
--- db/models/user.model.js
+++ db/models/user.model.js
@@ -1,7 +1,26 @@
+const { response } = require("../app.js");
 const db = require("../connection.js");
 
 exports.fetchUsers = () => {
   return db.query(`SELECT * FROM users`).then((response) => {
     return response.rows;
   });
+};
+
+exports.fetchSingleUser = (username) => {
+  const specialChars = /[`!@#$%^&*()+\=\[\]{};':"\\|,.<>\/?~]/;
+  if (specialChars.test(username))
+    return Promise.reject({ status: 400, msg: "bad request" });
+
+  return db
+    .query(`SELECT * FROM users WHERE username = $1;`, [username])
+    .then(({ rows }) => {
+      if (rows.length === 0) {
+        return Promise.reject({
+          status: 404,
+          msg: "no user matching that id",
+        });
+      }
+      return { user: rows[0] };
+    });
 };
