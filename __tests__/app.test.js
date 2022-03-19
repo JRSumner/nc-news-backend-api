3e218426f997f0cfdc5a6700d29a714cb163807e
JRSumner <joeraysumner@gmail.com>
2022-03-19 11:31:34 +0000

    collapse test functions

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -429,13 +429,26 @@ describe("DELETE: /api/comments/comment_id", () => {
   });
 });
 
-// describe.only("GET: /api", () => {
-//   test("should ", () => {
-//     return request(app)
-//       .get("/api")
-//       .expect(200)
-//       .then((res) => {
-//         expect(1).toEqual(1);
-//       });
-//   });
-// });
+describe("GET: /api", () => {
+  test("status 200: returns an object containing details of all the available endpoints", () => {
+    return request(app)
+      .get("/api")
+      .expect(200)
+      .then(({ body }) => {
+        expect(body).toEqual(
+          expect.objectContaining({
+            "GET /api": expect.any(Object),
+            "GET /api/topics": expect.any(Object),
+            "GET /api/articles": expect.any(Object),
+            "GET /api/articles/:article_id": expect.any(Object),
+            "PATCH /api/articles/:article_id": expect.any(Object),
+            "GET /api/articles/:article_id/comments": expect.any(Object),
+            "POST /api/articles/:article_id/comments": expect.any(Object),
+            "PATCH /api/comments/:comment_id": expect.any(Object),
+            "DELETE /api/comments/:comment_id": expect.any(Object),
+            "GET /api/users/:username": expect.any(Object),
+          })
+        );
+      });
+  });
+});
--- db/controllers/endpoints.contoller.js
+++ db/controllers/endpoints.contoller.js
@@ -1,11 +1,5 @@
 const { fetchEndpoints } = require("../models/endpoints.model");
 
 exports.getEndpoints = (req, res, next) => {
-  fetchEndpoints()
-    .then((response) => {
-      res.status(200).send(response);
-    })
-    .catch((err) => {
-      next(err);
-    });
+  res.status(200).send(fetchEndpoints());
 };
