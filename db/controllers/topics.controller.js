b5fa20f90eccc4208ea471049bc79e0a46da90e1
JRSumner <joeraysumner@gmail.com>
2022-03-22 20:45:08 +0000

    refactor test for GET: /api/topics

--- db/controllers/topics.controller.js
+++ db/controllers/topics.controller.js
@@ -1,12 +1,12 @@
 const res = require("express/lib/response");
 const { fetchTopics } = require("../models/topics.model.js");
 
-exports.getTopics = (req, res) => {
+exports.getTopics = (req, res, next) => {
   fetchTopics()
     .then((topicsArray) => {
       res.status(200).send({ topics: topicsArray });
     })
     .catch((err) => {
-      console.log(err);
+      next(err);
     });
 };
